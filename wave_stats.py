e16625d9eb8a2a3b3cd86d2fcdc089b4c7b89a90
CHRISTOPHER SHERWOOD <csherwood@usgs.gov>
2023-06-03 19:27:52 -0400

    sat night

--- wave_stats.py
+++ wave_stats.py
@@ -134,19 +134,23 @@ def calc_weighted_theta_mean_a1b1( a1, b1, spec1d, frequencies ):
     return thetamw
 
 
-def calc_sigma_theta_a1b1( a1, b1 ):
-    # directional spread (f) (degrees)
+def calc_sigma_theta_a1b1( a1, b1, spec1d, frequencies ):
+    # directional spread (degrees)
     # Herbers et al., eqn 3a; Le Merle et al., 2021 eqns. 5 & 6
-    s1 = (180./np.pi)*np.sqrt( 2. * ( 1.-np.sqrt( a1**2 + b1**2 ) ) )
+    a1bar = np.trapz(a1 * spec1d, frequencies) / np.trapz( spec1d, frequencies )
+    b1bar = np.trapz(b1 * spec1d,  frequencies) / np.trapz( spec1d, frequencies )
+    s1 = (180./np.pi)*np.sqrt( 2. * ( 1.-np.sqrt( a1bar**2 + b1bar**2 ) ) )
     s1[s1>=360.]=s1[s1>=360.]-360.
     s1[s1<0.]=s1[s1<0.]+360.
     return s1
 
 
-def calc_sigma_theta2_a2b2( a2, b2 ):
+def calc_sigma_theta2_a2b2( a2, b2, spec1d, frequencies ):
     # second estimate of directional spread (f) (degrees)
     # Herbers et al., eqn 3a; Ardhuin et al., eqns 5 & 6.
-    s2 = (180./np.pi)*np.sqrt( 0.5 * ( 1.-np.sqrt( a2**2 + b2**2 ) ) )
+    a2bar = np.trapz(a2 * spec1d, frequencies) / np.trapz( spec1d, frequencies )
+    b2bar = np.trapz(b2 * spec1d,  frequencies) / np.trapz( spec1d, frequencies )
+    s2 = (180./np.pi)*np.sqrt( 0.5 * ( 1.-np.sqrt( a2bar**2 + b2bar**2 ) ) )
     s2[s2>=360.]=s2[s2>=360.]-360.
     s2[s2<0.]=s2[s2<0.]+360.
     return s2
