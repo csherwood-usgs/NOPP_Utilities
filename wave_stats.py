b8b9764d1536ba5c1ccff69593ffec865f795993
CHRISTOPHER SHERWOOD <csherwood@usgs.gov>
2023-06-03 19:07:10 -0400

    sat night

--- wave_stats.py
+++ wave_stats.py
@@ -41,10 +41,11 @@ def to_moment( R1,R2, alpha1,alpha2 ):
     :return:
     """
     to_rad = np.pi / 180
+    eps = 1e-16
     angle1 = (270 - alpha1) * to_rad
     angle2 = (540 - 2 * alpha2) * to_rad
 
-    a0 = 1/(2*np.pi)
+    a0 = 1/(2*np.pi) + eps
     a1 =R1 * np.cos(angle1) / a0
     b1 =R1 * np.sin(angle1) / a0
     a2 =R2 * np.cos(angle2) / a0
@@ -52,12 +53,16 @@ def to_moment( R1,R2, alpha1,alpha2 ):
 
     return a1, b1, a2, b2
 
+
 def to_Fourier( Efth, frequencies, direction_radians, directional_bin_width_deg, faxis=0, daxis=1 ):
     """
     Convert 2d spectrum to Fourier coefficients
     Based on code from Isabel Houghton, Sofar
     """
-    Ef = integrate_in_direction(Efth, directional_bin_width_deg, daxis=daxis)
+    eps = 1e-16
+    Ef = integrate_in_direction(Efth, directional_bin_width_deg, daxis=daxis) + eps
+    print('shape of Ef: ',np.shape(Efth))
+    print('shape of Ef: ',np.shape(Ef))
 
     # each directional coefficient (a1, b1..) is a Fourier coefficient of the 2D WW3 spectrum,
     # normalized by the 1D (directionally-integrated) energy density spectrum
@@ -75,6 +80,19 @@ def to_Fourier( Efth, frequencies, direction_radians, directional_bin_width_deg,
     
     return Ef, a1, a2, b1, b2
 
+
+#### 1d statistics from 1d data
+
+def calc_m0_1d( spec1d, frequencies ):
+    return np.trapz( spec1d, frequencies )
+
+
+def calc_m1_1d( spec1d, frequencies ):
+    return np.trapz( spec1d * frequencies, frequencies )
+
+
+def calc_Hs_1d( spec1d, frequencies ):
+    return 4. * np.sqrt( calc_m0_1d( spec1d, frequencies ) )
 
 
 def calc_spec1d_2d( spec2d, directional_bin_width, daxis=1 ):
@@ -96,11 +114,24 @@ def calc_Qp_1d( spec1d, frequencies ):
     return 2.*np.trapz( spec1d**2, frequencies ) / (np.trapz( spec1d, frequencies ))**2 
 
 
-def calc_theta_mean_a1b1( a1, b1):
-    # mean direction (f) (degrees)
-    # Le Merle et al., 2021 eqn 5
-    thetam = (180./np.pi) * np.arctan2( b1, a1 )
+def calc_theta_mean_a1b1( a1, b1, frequencies ):
+    # mean direction (degrees)
+    # no weighting
+    # Le Merle et al., 2021 eqn 5; Isabels code, SoFar manual
+    a1bar = np.trapz(a1, frequencies)
+    b1bar = np.trapz(b1, frequencies)
+    thetam = 270. - (180./np.pi) * np.arctan2( b1bar, a1bar )
     return thetam
+
+
+def calc_weighted_theta_mean_a1b1( a1, b1, spec1d, frequencies ):
+    # mean direction (degrees)
+    # no weighting
+    # Le Merle et al., 2021 eqn 5; Isabels code, SoFar manual
+    a1bar = np.trapz(a1 * spec1d, frequencies) / np.trapz( spec1d, frequencies )
+    b1bar = np.trapz(b1 * spec1d,  frequencies) / np.trapz( spec1d, frequencies )
+    thetamw = 270. - (180./np.pi) * np.arctan2( b1bar, a1bar )
+    return thetamw
 
 
 def calc_sigma_theta_a1b1( a1, b1 ):
@@ -119,14 +150,6 @@ def calc_sigma_theta2_a2b2( a2, b2 ):
     s2[s2>=360.]=s2[s2>=360.]-360.
     s2[s2<0.]=s2[s2<0.]+360.
     return s2
-
-
-def calc_m0_1d( spec1d, frequencies ):
-    return np.trapz( spec1d, frequencies )
-
-
-def calc_m1_1d( spec1d, frequencies ):
-    return np.trapz( spec1d * frequencies, frequencies )
 
 
 def calc_FSPR_1d( spec1d, frequencies, TM02 ):
