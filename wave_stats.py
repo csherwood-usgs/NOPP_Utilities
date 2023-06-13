dcb2addd5f263b89fbd7aa6991a49000291ce4f9
CHRISTOPHER SHERWOOD <csherwood@usgs.gov>
2023-06-13 18:01:13 -0400

    nans!

--- wave_stats.py
+++ wave_stats.py
@@ -44,12 +44,17 @@ def to_moment( R1,R2, alpha1,alpha2 ):
     eps = 1e-16
     angle1 = (270 - alpha1) * to_rad
     angle2 = (540 - 2 * alpha2) * to_rad
-
+    
     a0 = 1/(2*np.pi) + eps
     a1 =R1 * np.cos(angle1) / a0
     b1 =R1 * np.sin(angle1) / a0
     a2 =R2 * np.cos(angle2) / a0
     b2 =R2 * np.sin(angle2) / a0
+    
+    a1 = np.squeeze( np.nan_to_num( a1 ) )
+    b1 = np.squeeze( np.nan_to_num( b1 ) )
+    a2 = np.squeeze( np.nan_to_num( a2 ) )
+    b2 = np.squeeze( np.nan_to_num( b2 ) )
 
     return a1, b1, a2, b2
 
@@ -118,9 +123,14 @@ def calc_theta_mean_a1b1( a1, b1, frequencies ):
     # mean direction (degrees)
     # no weighting
     # Le Merle et al., 2021 eqn 5; Isabels code, SoFar manual
+    a1 = np.squeeze( np.nan_to_num( a1 ) )
+    b1 = np.squeeze( np.nan_to_num( b1 ) )
+    
     a1bar = np.trapz(a1, frequencies)
     b1bar = np.trapz(b1, frequencies)
     thetam = 270. - (180./np.pi) * np.arctan2( b1bar, a1bar )
+    if(thetam>=360.): thetam = thetam -360.
+    if(thetam<0.): thetam = thetam +360.
     return thetam
 
 
@@ -128,18 +138,26 @@ def calc_weighted_theta_mean_a1b1( a1, b1, spec1d, frequencies ):
     # mean direction (degrees)
     # no weighting
     # Le Merle et al., 2021 eqn 5; Isabels code, SoFar manual
+    a1 = np.squeeze( np.nan_to_num( a1 ) )
+    b1 = np.squeeze( np.nan_to_num( b1 ) )
     a1bar = np.trapz(a1 * spec1d, frequencies) / np.trapz( spec1d, frequencies )
     b1bar = np.trapz(b1 * spec1d,  frequencies) / np.trapz( spec1d, frequencies )
     thetamw = 270. - (180./np.pi) * np.arctan2( b1bar, a1bar )
+    if(thetamw>=360.): thetamw = thetamw-360.
+    if(thetamw<0.): thetamw = thetamw+360.
     return thetamw
 
 
 def calc_sigma_theta_a1b1( a1, b1, spec1d, frequencies ):
     # directional spread (degrees)
     # Herbers et al., eqn 3a; Le Merle et al., 2021 eqns. 5 & 6
+    a1 = np.squeeze( np.nan_to_num( a1 ) )
+    b1 = np.squeeze( np.nan_to_num( b1 ) )
     a1bar = np.trapz(a1 * spec1d, frequencies) / np.trapz( spec1d, frequencies )
-    b1bar = np.trapz(b1 * spec1d,  frequencies) / np.trapz( spec1d, frequencies )
-    s1 = (180./np.pi)*np.sqrt( 2. * ( 1.-np.sqrt( a1bar**2 + b1bar**2 ) ) )
+    b1bar = np.trapz(b1 * spec1d, frequencies) / np.trapz( spec1d, frequencies )
+    coeff = 1. - np.sqrt( a1bar**2 + b1bar**2 )
+    if ( coeff )<0. : print('coeff:',coeff)
+    s1 = (180./np.pi) * np.sqrt( 2. * ( 1. - np.sqrt( a1bar**2 + b1bar**2 ) ) )
     big = np.argwhere(s1>=360.)
     if(s1>=360.): s1 = s1-360.
     if(s1<0.): s1 = s1+360.
@@ -149,6 +167,8 @@ def calc_sigma_theta_a1b1( a1, b1, spec1d, frequencies ):
 def calc_sigma_theta2_a2b2( a2, b2, spec1d, frequencies ):
     # second estimate of directional spread (f) (degrees)
     # Herbers et al., eqn 3a; Ardhuin et al., eqns 5 & 6.
+    a2 = np.squeeze( np.nan_to_num( a2 ) )
+    b2 = np.squeeze( np.nan_to_num( b2 ) )
     a2bar = np.trapz(a2 * spec1d, frequencies) / np.trapz( spec1d, frequencies )
     b2bar = np.trapz(b2 * spec1d,  frequencies) / np.trapz( spec1d, frequencies )
     s2 = (180./np.pi)*np.sqrt( 0.5 * ( 1.-np.sqrt( a2bar**2 + b2bar**2 ) ) )
